da92613537a9158d0d53500b0b9e3e9d7764a571
Nourhan Ibrahim <nourhan@localhost.localdomain>
2025-12-22 01:13:35 +0200

    + create_db.sh list_db.sh

---
+++ db_ops/create_db.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+DB_DIR="./databases"
+mkdir -p "$DB_DIR"
+
+create_database() {
+    read -r -p "Enter database name: " db_name
+
+    if [[ -z "$db_name" ]]
+    then
+            echo "Invalid database name."
+            echo "Must not be empty."
+            return
+    fi
+
+    if [[ "$db_name" == *" "* ]]
+    then
+            echo "Invalid database name."
+            echo "Must not conatin spaces."
+            return
+    fi
+
+    if [[ "$db_name" != [a-z]* || "$db_name" != [A-Z]* ]]
+    then
+            echo "Invalid database name."
+            echo "Must start with an alphabetic character."
+            return
+    fi
+
+    if [ -d "$DB_DIR/$db_name" ];
+    then
+        echo "Database '$db_name' already exists."
+    else
+        mkdir "$DB_DIR/$db_name"
+        echo "Database '$db_name' created successfully."
+    fi
+}
+create_database
+
