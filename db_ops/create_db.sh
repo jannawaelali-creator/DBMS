e9b22bad1bfc2a28508d584723c90b027cf5ec42
jannawaelali-creator <janna@LAPTOP-DMKLA76Q.home>
2025-12-28 17:40:06 +0200

    edit in create database fixing bugs

--- db_ops/create_db.sh
+++ db_ops/create_db.sh
@@ -2,37 +2,48 @@
 
 DB_DIR="./databases"
 mkdir -p "$DB_DIR"
-
+# UI Colors
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+CYAN='\033[0;36m'
+YELLOW='\033[1;33m'
+NC='\033[0m'   # Reset
 create_database() {
+          # Header
+    echo -e "${CYAN}+------------------------------+${NC}"
+    printf "${CYAN}| %-28s |${NC}\n" " Create Database"
+    echo -e "${CYAN}+------------------------------+${NC}"
+
+
     read -r -p "Enter database name: " db_name
 
     if [[ -z "$db_name" ]]
     then
-            echo "Invalid database name."
-            echo "Must not be empty."
+            echo -e "${RED} Invalid database name.${NC} "
+            echo -e  "${RED}  Must not be empty.${NC} "
             return
     fi
 
     if [[ "$db_name" == *" "* ]]
     then
-            echo "Invalid database name."
-            echo "Must not conatin spaces."
+            echo -e  " ${RED} Invalid database name. ${NC} "
+            echo -e  " ${RED} Must not conatin spaces . ${NC} "
             return
     fi
 
     if [[ "$db_name" != [a-zA-Z]* ]]
     then
-            echo "Invalid database name."
-            echo "Must start with an alphabetic character."
+            echo -e " ${RED} Invalid database name. ${NC} "
+            echo -e  "${RED} Must start with an alphabetic character.${NC} "
             return
     fi
 
     if [ -d "$DB_DIR/$db_name" ];
     then
-        echo "Database '$db_name' already exists."
+        echo -e  "${RED}  Database '$db_name' already exists.${NC} "
     else
         mkdir "$DB_DIR/$db_name"
-        echo "Database '$db_name' created successfully."
+        echo -e "${GREEN}  Database '$db_name' created successfully.${NC} "
     fi
 }
 
